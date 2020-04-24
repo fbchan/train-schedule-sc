e88dcb5d4413f11d6fd9874d0621458e1c985e1a
fbchan <foobang.chan@gmail.com>
2020-04-24 20:21:39 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var myText = req.query.mytext; //mytext is the name of your input box
+    res.send('Train Search:' +myText); 
   res.render('index', { title: 'Express' });
 });
 
